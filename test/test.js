e7bd745eddc0e86ef9e33a9430eb34353463d4f6
Edwin Rugoogamu <rugoogamuedwin5@gmail.com>
2023-06-27 18:10:24 +0100

    first commit

---
+++ test/test.js
@@ -0,0 +1,25 @@
+const assert = require("assert");
+const multiply = require("./multiply");
+
+
+describe("mulitply", () => {
+  it("1*1 = 1", () => {
+    assert.equal(multiply(1, 1), 1);
+  });
+
+  it("2*2 = 4", () => {
+    assert.equal(multiply(2, 2), 4);
+  });
+
+  it("3*3 = 9", () => {
+    assert.equal(multiply(3, 3), 9);
+  });
+
+  it("4*4 =16", () => {
+    assert.equal(multiply(4, 4), 16);
+  });
+
+  it("23*45 = 1035", () => {
+    assert.equal(multiply(23, 45), 1035);
+  });
+});
